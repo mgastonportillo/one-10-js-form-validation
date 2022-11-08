9926fd353eb391a845c392fd35171ac09baebbff
mgastonportillo <mgastonportillo@gmail.com>
2022-11-08 20:52:50 -0300

    complete class 4

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -4,10 +4,57 @@ export const validate = (input) => {
 	if (VALIDATORS[inputType]) {
 		VALIDATORS[inputType](input);
 	}
+
+	if (input.validity.valid) {
+		input.parentElement.classList.remove("input-container--invalid");
+		input.parentElement.querySelector(".input-message-error").innerHTML = "";
+	} else {
+		input.parentElement.classList.add("input-container--invalid");
+		input.parentElement.querySelector(".input-message-error").innerHTML =
+			displayErrorMessage(inputType, input);
+	}
 };
 
 const VALIDATORS = {
 	birth: (input) => validateBirth(input),
+};
+
+const ERROR_TYPES = [
+	"valueMissing",
+	"typeMismatch",
+	"patternMismatch",
+	"customError",
+];
+
+const ERROR_MESSAGES = {
+	name: {
+		valueMissing: "Name field cannot be empty",
+	},
+	email: {
+		valueMissing: "Email field cannot be empty",
+		typeMismatch: "Make sure you are entering the right email",
+	},
+	password: {
+		valueMissing: "Password field cannot be empty",
+		patternMismatch:
+			"6-12 characters. Must contain a lowercase letter, an uppercase letter, a number and cannot contain special characters",
+	},
+	birth: {
+		valueMissing: "Date of birth field cannot be empty",
+		customError: "Must be at least 18 years old",
+	},
+};
+
+// a
+
+const displayErrorMessage = (inputType, input) => {
+	let message = "";
+	ERROR_TYPES.forEach((error) => {
+		if (input.validity[error]) {
+			message = ERROR_MESSAGES[inputType][error];
+		}
+	});
+	return message;
 };
 
 // birth
@@ -16,7 +63,7 @@ const validateBirth = (input) => {
 	const userDate = new Date(input.value);
 	let message = "";
 	if (!validAge(userDate)) {
-		message = "You are underage. You cannot sign up.";
+		message = "Must be at least 18 years old";
 	}
 	input.setCustomValidity(message);
 };
